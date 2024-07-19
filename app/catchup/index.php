0599fcba3c5fe7011a8fc8748e6e972a3b607567
Mukesh Yadav <iamcoolmukesh@gmail.com>
2024-07-19 18:24:54 +0530

    Delete app directory

--- app/catchup/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// * Copyright 2021-2024 SnehTV, Inc.
-// * Licensed under MIT (https://github.com/mitthu786/TS-JioTV/blob/main/LICENSE)
-// * Created By : TechieSneh
-
-header("Location: ../");
-exit();
